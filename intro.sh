ec2416d69c9b266d6ffa834cdfa93232bf55139e
Bhushan Baskar <bhushanbaskar3@gmail.com>
2026-07-22 23:06:10 +0530

    learn create and edit file using powershell

---
+++ intro.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "================================
+Welcome to Shell Scripting
+================================
+
+Name      : Bhushan
+Branch    : Computer Science & Design
+College   : K.K. Wagh Engineering Institute
+
+Today is a great day to automate tasks!"
